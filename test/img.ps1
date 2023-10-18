b5e887efda54223f54fa88045b7085af50a5fe19
8LWXpg <105704427+8LWXpg@users.noreply.github.com>
2023-10-18 17:29:02 +0800

    add img script

---
+++ test/img.ps1
@@ -0,0 +1,18 @@
+# extract image from README code block and export to img folder
+Push-Location
+Set-Location $PSScriptRoot
+
+$template =
+'#import "../ansi-render.typ": *
+#set page(width: auto, height: auto, margin: 3pt)
+'
+$m = Get-Content '../README.md' -Raw | Select-String -Pattern '```typst([\s\S]*?)```' -AllMatches
+$m = $m.Matches.Captures
+for ($i = 1; $i -le $m.Count - 1; $i++) {
+	$content = $m[$i].Groups[1].Value
+	$file = "$i.typ"
+	$template + $content > "$i.typ"
+	typst c $file "../img/$i.png" --root ../ && Remove-Item $file
+}
+
+Pop-Location
