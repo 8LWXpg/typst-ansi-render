21664e4c8afcb05d840b637cc881c016fbdb771d
8LWXpg <105704427+8LWXpg@users.noreply.github.com>
2023-10-20 15:47:21 +0800

    lt

--- test/img.ps1
+++ test/img.ps1
@@ -7,7 +7,7 @@ $template =
 #set page(width: auto, height: auto, margin: 3pt)
 '
 $m = (Get-Content ../README.md -Raw | Select-String -Pattern '```typst([\s\S]*?)```' -AllMatches).Matches.Captures
-for ($i = 1; $i -le $m.Count - 1; $i++) {
+for ($i = 1; $i -lt $m.Count; $i++) {
 	$content = $m[$i].Groups[1].Value
 	$file = "$i.typ"
 	$template + $content > "$i.typ"
