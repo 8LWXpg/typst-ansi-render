b51c52b96716ec570f2938cef00749095054f9b3
8LWXpg <peng200208@gmail.com>
2026-05-07 17:34:22 +0800

    documentation, change to tagged url, update bump script

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -2,13 +2,13 @@
 
 Thank you for your interest in contributing to this project! This document outlines the steps you should follow to contribute to the project.
 
-## Adding a new theme
+## Adding a New Theme
 
 1. Fork the repository on GitHub.
 2. Add your theme to the `terminal-themes` dictionary.
 3. Add theme preview to the `test/themes.typ` file.
 4. Make sure you have `Cascadia Code` font installed and compile the `test/themes.typ` file (you can use `compile themes` task in VS Code).
 
-## Code formatting
+## Code Formatting
 
 Please use kebab-case and two spaces for indentation.
--- bump.ps1
+++ bump.ps1
@@ -5,14 +5,15 @@ param(
 # validate version
 if ($version -match '^\d+\.\d+\.\d+$') {
 	Write-Host -ForegroundColor Blue "Bumping to version $version"
-} else {
+}
+else {
 	Write-Host -ForegroundColor Red "Invalid version $version"
 	exit 1
 }
 
-$content = cat ./README.md | %{$_ -replace '"@preview/ansi-render:(\d+\.\d+\.\d+)"', "`"@preview/ansi-render:$version`""}
-Set-Content ./README.md $content
-$content = cat ./typst.toml | %{$_ -replace 'version = "(\d+\.\d+\.\d+)"', "version = `"$version`""}
-Set-Content ./typst.toml $content
+$lastTag = (git describe --tags --abbrev=0).Substring(1)
+
+(Get-Content ./README.md) -replace [regex]::Escape($lastTag), $version | Set-Content ./README.md
+(Get-Content ./typst.toml) -replace [regex]::Escape($lastTag), $version | Set-Content ./typst.toml
 
 Write-Host -ForegroundColor Red "Don't forget to write a changelog!"
